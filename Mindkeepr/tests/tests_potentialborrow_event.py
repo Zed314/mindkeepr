1c7a880a6d31884a1f629d6f1c6728ff31594042
Ziggy Vergne <ziggy.vergne@gmail.com>
2021-11-27 00:58:48 +0000

    Refactoring test potentialborrow event

--- Mindkeepr/tests/tests_potentialborrow_event.py
+++ Mindkeepr/tests/tests_potentialborrow_event.py
@@ -1,28 +1,37 @@
 from django.test import TestCase
-from django.test import Client
-from django.contrib.auth.models import AnonymousUser,User
-from rest_framework.test import APIRequestFactory
+from django.contrib.auth.models import User
 from datetime import date
 import datetime
-from Mindkeepr.views import EventsView
-from Mindkeepr.views.events.borrowing_view import BorrowingsView
 from Mindkeepr.models.events.buy_event import BuyEvent
 from Mindkeepr.models.events.borrow_event import BorrowEvent, PotentialBorrowEvent
 from Mindkeepr import models
 from django.contrib.auth.models import Permission
 
-class APITestCase(TestCase):
+class ModelTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_superuser('mindkeepr', 'mindkeepr@example.fr', 'admin')
         self.dumb_user = User.objects.create_user('benoit', 'benoit@example.fr')
-        self.borrow_allowed_user = User.objects.create_user('zigzag', 'zigzag@example.fr')
-        self.borrow_allowed_user.user_permissions.add(Permission.objects.get(codename='add_borrowevent'))
-        self.borrow_allowed_user.user_permissions.add(Permission.objects.get(codename='add_potentialborrowevent'))
+        #self.borrow_allowed_user = User.objects.create_user('zigzag', 'zigzag@example.fr')
+        #self.borrow_allowed_user.user_permissions.add(Permission.objects.get(codename='add_borrowevent'))
+        #self.borrow_allowed_user.user_permissions.add(Permission.objects.get(codename='add_potentialborrowevent'))
         self.location = models.Location.objects.create(name = "Location 1")
         self.machine = models.elements.Machine.objects.create(name = "Machine",description="A machine")
         self.machine2 = models.elements.Machine.objects.create(name = "Machine 2",description="Another machine")
         self.machine3 = models.elements.Machine.objects.create(name = "Machine 3",description="Another machine, again")
 
+    def create_potential_borrow(self,object,beg,end,expected_result=True):
+        pborrow = PotentialBorrowEvent()
+        pborrow.element = object
+        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=beg)
+        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=end)
+        if not expected_result:
+            try:
+                pborrow.save()
+                self.assertEqual(True,False)
+            except ValueError:
+                pass #OK!
+        else:
+            pborrow.save()
 
     def test_potential_borrow(self):
         buy = BuyEvent()
@@ -33,11 +42,8 @@ class APITestCase(TestCase):
         buy.element = self.machine
         buy.save()
 
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=1)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=2)
-        pborrow.save()
+
+        self.create_potential_borrow(self.machine,1,2)
         #self.machine
         intervals = self.machine.potential_borrow_intervals()
         self.assertEqual(intervals[0][0], date.today() +  datetime.timedelta(days=1))
@@ -46,11 +52,7 @@ class APITestCase(TestCase):
         intervals = self.machine.borrow_intervals()
         self.assertEqual(len(intervals), 0)
 
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=3)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=5)
-        pborrow.save()
+        self.create_potential_borrow(self.machine,3,5)
 
         intervals = self.machine.potential_borrow_intervals()
         self.assertEqual(intervals[0][0], date.today() +  datetime.timedelta(days=1))
@@ -58,24 +60,9 @@ class APITestCase(TestCase):
         self.assertEqual(intervals[1][0], date.today() +  datetime.timedelta(days=3))
         self.assertEqual(intervals[1][1], date.today() +  datetime.timedelta(days=5))
 
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=3)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=5)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
+        self.create_potential_borrow(self.machine,3,5,False)
         self.assertEqual(len(self.machine.potential_borrow_intervals()),2)
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=5)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=7)
-        pborrow.save()
+        self.create_potential_borrow(self.machine,5,7)
 
         intervals = self.machine.potential_borrow_intervals()
         self.assertEqual(intervals[0][0], date.today() +  datetime.timedelta(days=1))
@@ -85,106 +72,17 @@ class APITestCase(TestCase):
         self.assertEqual(intervals[2][0], date.today() +  datetime.timedelta(days=5))
         self.assertEqual(intervals[2][1], date.today() +  datetime.timedelta(days=7))
 
+        self.create_potential_borrow(self.machine,10,10,False)
+        self.create_potential_borrow(self.machine,10,11)
+        self.create_potential_borrow(self.machine,10,11,False)
+        self.create_potential_borrow(self.machine,13,12,False)
+        self.create_potential_borrow(self.machine,2,10,False)
+        self.create_potential_borrow(self.machine,0,2,False)
+        self.create_potential_borrow(self.machine,30,40)
+        self.create_potential_borrow(self.machine,25,35,False)
+        self.create_potential_borrow(self.machine,32,38,False)
+        self.create_potential_borrow(self.machine,38,41,False)
 
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=10)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=10)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=10)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=11)
-        pborrow.save()
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=10)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=11)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=13)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=12)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=2)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=10)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today()
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=2)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today()  +  datetime.timedelta(days=30)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=40)
-        pborrow.save()
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today()  +  datetime.timedelta(days=25)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=35)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today()  +  datetime.timedelta(days=32)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=38)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine
-        pborrow.scheduled_borrow_date = date.today()  +  datetime.timedelta(days=38)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=41)
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
 
     def test_potential_borrow_and_borrow(self):
         buy = BuyEvent()
@@ -212,75 +110,14 @@ class APITestCase(TestCase):
 
         # attempt an overlapping reservation
 
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today()
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=3)
-
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today()
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=5)
-
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today()
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=1)
-
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today() + datetime.timedelta(days=2)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=4)
-
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
-
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today() + datetime.timedelta(days=2)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=3)
-
-        #should fail
-        try:
-            pborrow.save()
-            self.assertEqual(True,False)
-        except ValueError:
-            pass #OK!
+        self.create_potential_borrow(self.machine2,0,3,False)
+        self.create_potential_borrow(self.machine2,0,5,False)
+        self.create_potential_borrow(self.machine2,0,1,False)
+        self.create_potential_borrow(self.machine2,2,4,False)
+        self.create_potential_borrow(self.machine2,2,3,False)
 
         # overlapping is allowed on ending day
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today() + datetime.timedelta(days=3)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=4)
-
-        pborrow.save()
+        self.create_potential_borrow(self.machine2,3,4)
 
         intervals = self.machine2.all_borrow_intervals(date.today(),date.today() +  datetime.timedelta(days=3))
         self.assertEqual(intervals["borrows"][0][0].date(), date.today())
@@ -290,11 +127,8 @@ class APITestCase(TestCase):
 
 
         # Non overlapping (just in case !)
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine2
-        pborrow.scheduled_borrow_date = date.today() + datetime.timedelta(days=5)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=10)
-        pborrow.save()
+        self.create_potential_borrow(self.machine2,5,10)
+
         #change date to see new potential interval
         intervals = self.machine2.all_borrow_intervals(date.today(),date.today() +  datetime.timedelta(days=10))
         self.assertEqual(intervals["potential"][1][0], date.today() + datetime.timedelta(days=5))
@@ -310,11 +144,7 @@ class APITestCase(TestCase):
         buy.element = self.machine3
         buy.save()
 
-        pborrow = PotentialBorrowEvent()
-        pborrow.element = self.machine3
-        pborrow.scheduled_borrow_date = date.today() +  datetime.timedelta(days=0)
-        pborrow.scheduled_return_date = date.today() +  datetime.timedelta(days=10)
-        pborrow.save()
+        self.create_potential_borrow(self.machine3,0,10)
 
         borrow = BorrowEvent()
         borrow.element = self.machine3
