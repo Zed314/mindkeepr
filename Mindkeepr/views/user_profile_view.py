88104b9e039517f1768c6857cccea572cb69a706
Zed314 <22842741+Zed314@users.noreply.github.com>
2022-03-09 23:44:48 +0000

    Profile improvements

--- Mindkeepr/views/user_profile_view.py
+++ Mindkeepr/views/user_profile_view.py
@@ -5,16 +5,23 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
 
+
 class UserProfileUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = UserProfile
     form_class = UserProfileForm
     template_name = "profile/profile-edit-modal.html"
     permission_required = "Mindkeepr.change_userprofile"
     queryset = UserProfile.objects.all()
-
     def get_success_url(self):
         return reverse_lazy('view_profile', kwargs={'pk': self.object.pk})
 
     def form_valid(self, form):
         # TODO check user
         return super(UserProfileUpdate, self).form_valid(form)
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='staff').exists():
+            return super().get_queryset().all()
+        return super().get_queryset().filter(
+            pk=self.request.user.pk
+        )
