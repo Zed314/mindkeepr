f5fcc4db4276423658732ee57fcdbd7281a221ba
Zed314 <22842741+Zed314@users.noreply.github.com>
2022-03-13 19:16:07 +0000

    Update machines to add VideoGames

--- Mindkeepr/serializers/elements/videogame.py
+++ Mindkeepr/serializers/elements/videogame.py
@@ -8,10 +8,13 @@ from ..serializer_factory import SerializerFactory
 @SerializerFactory.register("VideoGame")
 class VideoGameSerializer(ElementFieldMixin, serializers.HyperlinkedModelSerializer):
     custom_id_display = serializers.CharField(read_only=True)
+    platform_display = serializers.SerializerMethodField(read_only=True, source='get_platform_display')
     class Meta:
         model = VideoGame
-        fields = ElementSerializer.Meta.fields + ("custom_id_display","barcode_effective")
+        fields = ElementSerializer.Meta.fields + ("custom_id_display","platform","platform_display","barcode_effective")
         depth = 1
+    def get_platform_display(self,obj):
+            return obj.get_platform_display()
 
     def create(self, validated_data):
         category = self.get_category(validated_data)
